c5a216a73ce194e5ace4d104952f52b93af03762
Ravi Wu <raviwu@gmail.com>
2018-12-10 16:18:18 +0800

    Separate modules to data and web
    https://github.com/raviwu/sfg-pet-clinic/issues/1
    
    Recompose the project structure so that modules contain their
    own concerns.
    
    Use pom package on the root pom and modules with jar.

--- src/main/java/org/lwstudio/sfgpetclinic/model/Owner.java
+++
@@ -1,5 +0,0 @@
-package org.lwstudio.sfgpetclinic.model;
-
-public class Owner extends Person {
-
-}
--- src/main/java/org/lwstudio/sfgpetclinic/model/Vet.java
+++
@@ -1,5 +0,0 @@
-package org.lwstudio.sfgpetclinic.model;
-
-public class Vet extends Person {
-
-}
